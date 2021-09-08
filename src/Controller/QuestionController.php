aee9016ec134f6b3f16b3ee3615966f239a299bd
Juan Egido <juanpabloegido@gmail.com>
2021-09-08 20:22:13 +0200

    Added some Twiggy goodness

---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuestionController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+
+    public function homepage(): Response
+    {
+        return new Response('What a bewitching controller we have conjured!');
+    }
+
+    /**
+     * @Route ("/questions/{slug}")
+     */
+    public function show($slug): Response
+    {
+
+        $answers = [
+            'Make sure your car is sitting perfectly still :)',
+            'Honestly, I like furry shows better than my cat',
+            'Maybe... try saying the spell backwards',
+        ];
+
+
+        return $this->render('question/show.html.twig',
+        [
+            'question' => ucwords(str_replace('-',' ', $slug )),
+            'answers' => $answers
+        ]);
+
+    }
+}
